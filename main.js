82d6067ecd000e7a83ec1271ac6afd7fe28fc3be
leonardpaulus <leonard.paulus@agenturignis.de>
2021-10-11 17:20:36 +0200

    Add JavaScript functions on click

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const myQuestion = document.querySelector('.question');
-myQuestion.textContent = 'Is JavaScript difficult?';
+const correctAnswer = false;
+
+const yesAlert = document.querySelector('.yes');
+const noAlert = document.querySelector('.no');
+
+yesAlert.onclick = function () {
+if (correctAnswer === true) {
+alert("This is correct");
+} else {
+    alert("This is incorrect");
+}
+};
+
+noAlert.onclick = function () {
+    if (correctAnswer === false) {
+    alert("This is correct");
+    } else {
+        alert("This is incorrect");
+    }
+    };
